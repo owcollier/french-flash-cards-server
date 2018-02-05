8a2219728cb269a29a104a16968bbeb9527a112a
Owcollier <owcollier@gmail.com>
2018-02-05 17:34:34 -0500

    attempting to debug post endpoint, added test

--- auth/router.js
+++ auth/router.js
@@ -20,6 +20,7 @@ const localAuth = passport.authenticate('local', {session: false});
 router.use(bodyParser.json());
 
 router.post('/login', localAuth, (req, res) => {
+  console.log('here');
   const authToken = createAuthToken(req.user.serialize());
   res.json({authToken});
 });
