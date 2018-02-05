dbb8c34638283acaf2fa871425458b925a0537e7
Owcollier <owcollier@gmail.com>
2018-02-05 14:47:20 -0500

    added linter-friendly stuff

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const {router} = require('./router');
 const {localStrategy, jwtStrategy} = require('./strategies');
 
--- users/models.js
+++ users/models.js
@@ -1,42 +1,44 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+'use strict';
 
-mongoose.Promise = global.Promise
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
 
 const userSchema = mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    firstName: {
-        type: String,
-        default: ''
-    },
-    lastName: {
-        type: String,
-        default: ''
-    }
-})
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  }
+});
 
 userSchema.methods.serialize = () => ({
-    userName: this.userName || '',
-    firstName: this.firstName || '',
-    lastName: this.lastName || ''
-})
+  userName: this.userName || '',
+  firstName: this.firstName || '',
+  lastName: this.lastName || ''
+});
 
 userSchema.methods.validatePassword = (password) => {
-    return bcrypt.compare(password, this.password)
-}
+  return bcrypt.compare(password, this.password);
+};
 
 userSchema.methods.hashPassword = (password) => {
-    return bcrypt.hash(password, 10)
-}
+  return bcrypt.hash(password, 10);
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = { User }
+module.exports = { User };
